74a2ee77b890a4b1204e5a1604222f525e14d34a
Victor Contreras <contreras3991@gmail.com>
2025-07-14 12:01:40 -0300

    add create table task

---
+++ apache-airflow/airflow-with-marc/dags/user_processing.py
@@ -0,0 +1,21 @@
+from airflow.sdk import dag
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+@dag
+def user_processing():
+    
+    create_table = SQLExecuteQueryOperator(
+        task_id='create_table',
+        conn_id='postgres',
+        sql="""
+        CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY,
+            first_name VARCHAR(255),
+            last_name VARCHAR(255),
+            email VARCHAR(255),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+    )
+
+user_processing()
