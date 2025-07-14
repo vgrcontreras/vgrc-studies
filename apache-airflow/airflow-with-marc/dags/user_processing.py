f8232f0d66cf8d089a933accd63320dd06d41e8f
Victor Contreras <contreras3991@gmail.com>
2025-07-14 14:29:15 -0300

    feat: add extract user info task

--- apache-airflow/airflow-with-marc/dags/user_processing.py
+++ apache-airflow/airflow-with-marc/dags/user_processing.py
@@ -1,5 +1,6 @@
-from airflow.sdk import dag
+from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sensors.base import PokeReturnValue
 
 @dag
 def user_processing():
@@ -17,5 +18,32 @@ def user_processing():
         )
         """
     )
+
+    @task.sensor(poke_interval=30, timeout=300)
+    def is_api_available() -> PokeReturnValue:
+        import requests
+
+        response = requests.get('https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json')
+
+        if response.status_code == 200:
+            condition = True
+            fake_user = response.json()
+        else:
+            condition = False
+            fake_user = None
+
+        return PokeReturnValue(is_done=condition, xcom_value=fake_user)
+    
+    @task
+    def extract_user(fake_user):
+        return {
+            "id": fake_user['id'],
+            "first_name": fake_user['personalInfo']['firstName'],
+            "last_name": fake_user['personalInfo']['lastName'],
+            "email": fake_user['personalInfo']['email']
+        }
+    
+    fake_user = is_api_available()
+    extract_user(fake_user)
 
 user_processing()
